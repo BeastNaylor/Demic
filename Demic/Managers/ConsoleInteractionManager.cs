bfdd2f3cf54dfe1b763e1442a7d6c952281260ca
Ryan Naylor <Ryan.Naylor@hs2.org.uk>
2017-10-11 16:25:40 +0100

    Add Project w/ InteractionManager

---
+++ Demic/Managers/ConsoleInteractionManager.cs
@@ -0,0 +1,54 @@
+﻿using Demic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demic.Managers
+{
+    class ConsoleInteractionManager : IInteractionManager
+    {
+        private OutputLevel _minimumLevel;
+
+        public ConsoleInteractionManager(OutputLevel level)
+        {
+            _minimumLevel = level;
+        }
+
+        public string ReadInput(string message, List<string> options)
+        {
+            bool inputValid = false;
+            string input;
+            do
+            {
+                this.OutputContent(message);
+                this.OutputContent("[Options are: " + String.Join(", ", options.ToArray()) + "]");
+                input = Console.ReadLine();
+                this.OutputContent(input, OutputLevel.Debug);
+                if (options.Contains(input.ToUpper()))
+                {
+                    inputValid = true;
+                }
+                else
+                {
+                    this.OutputContent("Invalid selection. Please try again.");
+                }
+            } while (!inputValid);
+            return input;
+        }
+
+        public void OutputContent(string message, OutputLevel level = OutputLevel.Information)
+        {
+            if (_minimumLevel <= level)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        internal void Pause()
+        {
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Demic/Program.cs
@@ -0,0 +1,21 @@
+﻿using Demic.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interactionManager = new ConsoleInteractionManager(Demic.Enums.OutputLevel.Information);
+            interactionManager.OutputContent("Hello");
+            var input = interactionManager.ReadInput("Select a Letter", new List<string>() { "A", "B" });
+            interactionManager.OutputContent(String.Format("Selected {0}", input));
+            interactionManager.Pause();
+        }
+    }
+}
