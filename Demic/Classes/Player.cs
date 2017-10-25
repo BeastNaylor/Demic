6f586620da01e6b1584b4175e823ace27ca65a2c
Ryan Naylor <Ryan.Naylor@hs2.org.uk>
2017-10-25 11:07:25 +0100

    Added the Player Class and Player Role objects

---
+++ Demic/Classes/Player.cs
@@ -0,0 +1,27 @@
+﻿using Demic.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demic.Classes
+{
+    class Player
+    {
+        public readonly string Name;
+        public readonly PlayerRole Role;
+
+        public Player(string name, PlayerRole role)
+        {
+            this.Name = name;
+            this.Role = role;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} the {1}", Name, Role.ToString());
+        }
+
+    }
+}
